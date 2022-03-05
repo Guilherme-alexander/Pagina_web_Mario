e9b20f313374076816ad98322f995b9318ac124a
alanabarbosa <itsalanayall@hotmail.com>
2022-03-04 22:15:05 -0300

    Adiciona animacoes e melhora o menu mobile

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 var menuIcon = document.querySelector('i.las.la-bars');
 var listMenu = document.querySelector(' nav.menu-mobile ul');
 var main = document.querySelector('main')
-
+const iconClose = document.querySelector('.menu-mobile i')
 
 menuIcon.addEventListener('click',()=>{ 
     listMenu.classList.toggle('active')
+    if (listMenu.classList.contains('active')) {
+        iconClose.setAttribute('class', 'las la-times')
+    } else {
+        iconClose.setAttribute('class', 'las la-bars')
+    }
 })
-
 
 main.addEventListener('click',()=>{
     listMenu.classList.remove('active')
 })
-
-    
-
-
-
- 
