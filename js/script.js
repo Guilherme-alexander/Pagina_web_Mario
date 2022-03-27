71920748642b054d35838ac0d0ddb4f5e102aba3
Guilherme-alexander <guilhermealexanderdsn@gmail.com>
2022-03-27 18:40:04 -0300

    add page loid is pokemon update dark mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-var menuIcon = document.querySelector('i.las.la-bars');
-var listMenu = document.querySelector(' nav.menu-mobile ul');
-var main = document.querySelector('main')
+let menuIcon = document.querySelector('i.las.la-bars');
+let listMenu = document.querySelector(' nav.menu-mobile ul');
+let main = document.querySelector('main')
+
 const iconClose = document.querySelector('.menu-mobile i')
 
 menuIcon.addEventListener('click',()=>{ 
@@ -14,4 +15,17 @@ menuIcon.addEventListener('click',()=>{
 
 main.addEventListener('click',()=>{
     listMenu.classList.remove('active')
+})
+
+//Dark Mode 
+let body = document.querySelector('body');
+let change = document.querySelector('.change');
+
+change.addEventListener('click',function(){
+    if(body.classList.contains('dark')){
+        body.classList.remove('dark')
+        
+    }else{
+        body.classList.add('dark')
+    }
 })
